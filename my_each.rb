a99701ddcc0c33c6c3dfd8c1755c2935d6ff7f18
Nathan Lewis <n.lewis.perc@gmail.com>
2020-11-25 03:07:54 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,16 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection)
+  if block_given?
+    i = 0
+    while i < 10 do
+      yield(i)
+      i += 1
+    end
+  else
+  end
 end
+
+=begin
+my_each(collection) do |i|
+  puts i
+end
+=end
