4205fd108409e70b6ea18e225ae64d21d377fd3d
Nathan Lewis <n.lewis.perc@gmail.com>
2020-11-25 03:08:49 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,16 +1,11 @@
 def my_each(collection)
   if block_given?
     i = 0
-    while i < 10 do
-      yield(i)
+    while i < array.length
+      yield array[i]
       i += 1
     end
+    array
   else
   end
 end
-
-=begin
-my_each(collection) do |i|
-  puts i
-end
-=end
